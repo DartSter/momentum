e38fac00c563f1eef22babb8ae2c32539e02230c
dartster <ster1904@gmail.com>
2023-02-01 21:27:47 +0200

    feat:add code for control service btns and cards

--- plants/js/main.js
+++ plants/js/main.js
@@ -16,28 +16,52 @@ console.log(`Самооценка:
 
 // burgerMenu
 
-const menuBtn = document.getElementById('menu-btn')
-const navList = document.getElementById('nav-list')
-const main = document.getElementById('main')
-const navItem = document.querySelectorAll('.nav-item')
+const menuBtn = document.getElementById("menu-btn");
+const navList = document.getElementById("nav-list");
+const main = document.getElementById("main");
+const navItem = document.querySelectorAll(".nav-item");
 
 function menu() {
-    navList.classList.toggle('active-menu')
-    menuBtn.classList.toggle('active-btn')
+  navList.classList.toggle("active-menu");
+  menuBtn.classList.toggle("active-btn");
 }
-    
+
 function hideMenu() {
-   
-    setTimeout(() => {
-        navList.classList.remove('active-menu')
-        menuBtn.classList.remove('active-btn')  
-   },500)
-    
+  setTimeout(() => {
+    navList.classList.remove("active-menu");
+    menuBtn.classList.remove("active-btn");
+  }, 500);
 }
 
-menuBtn.addEventListener('click', menu)
-main.addEventListener('click', menu)
+menuBtn.addEventListener("click", menu);
+main.addEventListener("click", menu);
 
-navItem.forEach(item => {
-    item.addEventListener('click', hideMenu)
+navItem.forEach((item) => {
+  item.addEventListener("click", hideMenu);
+});
+
+//service buttons
+
+const serviceBtns = document.querySelector(".service-controls-btns");
+const serviceCards = document.querySelectorAll(".service-card");
+const serviceCardsControl = {
+  garden: false,
+  lawn: false,
+  planting: false,
+};
+
+serviceBtns.addEventListener("click", (event) => {
+  let key = event.target.dataset.category;
+  event.target.classList.toggle("active-btn");
+
+  serviceCardsControl[key] = !serviceCardsControl[key];
+  serviceCards.forEach((item) => {
+    const category = item.dataset.category;
+    serviceCardsControl[category]
+      ? item.classList.remove("blur")
+      : item.classList.add("blur");
+  });
+  if (Object.values(serviceCardsControl).every((i) => !i)) {
+    serviceCards.forEach((item) => item.classList.remove("blur"));
+  }
 });
